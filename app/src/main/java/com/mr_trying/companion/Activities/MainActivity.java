1994ff45c7a4acb42b11e6cb4577669a99f3676c
DenTheRipper <hckrden@gmail.com>
2021-09-18 00:06:50 +1000

    fix after fix v5

--- app/src/main/java/com/mr_trying/companion/Activities/MainActivity.java
+++ app/src/main/java/com/mr_trying/companion/Activities/MainActivity.java
@@ -1,9 +1,12 @@
 package com.mr_trying.companion.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.mr_trying.companion.Adapters.MainMenuAdapter;
 import com.mr_trying.companion.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.setAdapter(new MainMenuAdapter(getApplicationContext()));
     }
 }
--- app/src/main/java/com/mr_trying/companion/Adapters/MainMenuAdapter.java
+++ app/src/main/java/com/mr_trying/companion/Adapters/MainMenuAdapter.java
@@ -1,6 +1,8 @@
 package com.mr_trying.companion.Adapters;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,9 +11,11 @@ import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.mr_trying.companion.R;
 
-public class MainMenuAdapter extends RecyclerView.Adapter<MainMenuAdapter.ViewHoder> {
+public class MainMenuAdapter extends RecyclerView.Adapter<MainMenuAdapter.ViewHolder> {
 
     private final Context context;
 
@@ -21,14 +25,16 @@ public class MainMenuAdapter extends RecyclerView.Adapter<MainMenuAdapter.ViewHo
 
     @NonNull
     @Override
-    public ViewHoder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        if (viewType == 0) return new ViewHoder(LayoutInflater.from(context).inflate(R.layout.menu_vertical_pt1, parent, false));
-        else return new ViewHoder(LayoutInflater.from(context).inflate(R.layout.menu_vertical_pt2, parent, false));
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == 0) return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.menu_vertical_pt1, parent, false));
+        else return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.menu_vertical_pt2, parent, false));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHoder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        if (position == 0) {
 
+        }
     }
 
     @Override
@@ -42,12 +48,22 @@ public class MainMenuAdapter extends RecyclerView.Adapter<MainMenuAdapter.ViewHo
         else return 1;
     }
 
-    public static class ViewHoder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
 
         public ImageView journal;
         public ImageView translator;
-        public ViewHoder(@NonNull View itemView) {
+        public ImageView bonus;
+        public ImageView bank;
+        public ImageView fromWindow;
+
+        public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            journal = itemView.findViewById(R.id.journal);
+            translator = itemView.findViewById(R.id.translator);
+            bonus = itemView.findViewById(R.id.rjd_bonus);
+            bank = itemView.findViewById(R.id.pricing);
+            fromWindow = itemView.findViewById(R.id.russia_from_window);
         }
     }
 }
