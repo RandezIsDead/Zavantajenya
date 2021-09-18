8c68d14094c2dabc1a7d7b43bfbe5274bc73da83
Tish <bav.tim@mail.ru>
2021-09-18 07:23:33 +0300

    12345

--- app/src/main/java/com/mr_trying/companion/Activities/SecondaryActivities/JournalActivity.java
+++ app/src/main/java/com/mr_trying/companion/Activities/SecondaryActivities/JournalActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.mr_trying.companion.R;
 
@@ -14,7 +15,14 @@ public class JournalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal);
-
+        TextView flight_number = findViewById(R.id.flight_number);
+        TextView car_number = findViewById(R.id. car_number);
+        TextView seat_number = findViewById(R.id.seat_number);
+        TextView from_station = findViewById(R.id.from_station);
+        TextView to_station = findViewById(R.id.to_station);
+        TextView arrival_time = findViewById(R.id.arrival_time);
+        TextView sending_time = findViewById(R.id.sending_time);
+        TextView passenger_status = findViewById(R.id.passenger_status);
         ImageView close = findViewById(R.id.close);
         close.setOnClickListener(v -> {
             finish();
